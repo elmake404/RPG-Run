1eb526e26307e4fb5ffe319d088d7f3e370bdac1
Valentin <kibirevvalik202@gmail.com>
2021-08-09 13:29:27 +0300

    [add] Project

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    private PlayerMove _target;
+    private Vector3 _velocity, _offset;
+    [SerializeField]
+    private float _smoothTime;
+
+
+    private void Start()
+    {
+        _target = FindObjectOfType<PlayerMove>();
+        _offset = transform.position - _target.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (GameStage.IsGameFlowe)
+        {
+            Vector3 NextPosCamera = (_target.transform.position + _offset);
+            NextPosCamera.x = transform.position.x;
+            transform.position = Vector3.SmoothDamp(transform.position, NextPosCamera, ref _velocity, _smoothTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMove : MonoBehaviour
+{
+    private Vector3 _startTouchPos, _currentPosPlayer, _targetPosPlayer, _startPosPlayer;
+    private Camera _cam;
+    [SerializeField]
+    private float _lateralSpeed, _runningSpeed, _boosterAccelerationPercentage;
+    [SerializeField]
+    private float _horizontalLimit;
+
+    private void Start()
+    {
+        _startPosPlayer = transform.position;
+        _targetPosPlayer = transform.position;
+        _cam = Camera.main;
+    }
+    private void Update()
+    {
+        if (TouchUtility.TouchCount > 0)
+        {
+            Touch touch = TouchUtility.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+                _currentPosPlayer = transform.position;
+
+                _startTouchPos = (_cam.transform.position - ((ray.direction) *
+                        ((_cam.transform.position - transform.position).z / ray.direction.z)));
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+
+                if (_startTouchPos == Vector3.zero)
+                {
+                    _startTouchPos = (_cam.transform.position - ((ray.direction) *
+                            ((_cam.transform.position - transform.position).z / ray.direction.z)));
+                }
+
+                _targetPosPlayer = _currentPosPlayer + ((_cam.transform.position - ((ray.direction) *
+                        ((_cam.transform.position - transform.position).z / ray.direction.z))) - _startTouchPos);
+            }
+        }
+        else
+        {
+            _targetPosPlayer = transform.position;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        //if (GameStage.IsGameFlowe)
+        //{
+            Vector3 PosX = transform.position;
+            PosX.x = CheckLimmit(_targetPosPlayer);
+            transform.position = Vector3.MoveTowards(transform.position, PosX, _lateralSpeed);
+
+            transform.Translate(Vector3.forward * _runningSpeed);
+        //}
+    }
+
+    private float CheckLimmit(Vector3 target)
+    {
+        if (target.x > _startPosPlayer.x + _horizontalLimit)
+        {
+            target.x = _startPosPlayer.x + _horizontalLimit;
+        }
+        else if (target.x < _startPosPlayer.x - _horizontalLimit)
+        {
+            target.x = _startPosPlayer.x - _horizontalLimit;
+        }
+        return target.x;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position - Vector3.right * _horizontalLimit, transform.position + Vector3.right * _horizontalLimit);
+    }
+}
